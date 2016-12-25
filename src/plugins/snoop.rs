10d24d5a97ce05ffad403298e209cd13d0df1608
John-John Tedro <udoprog@tedro.se>
2016-12-25 10:26:07 +0100

    initial code to setup server in snoop

--- src/plugins/snoop.rs
+++ src/plugins/snoop.rs
@@ -3,6 +3,12 @@
 use ::errors::*;
 use ::plugin::*;
 
+use std::net::SocketAddr;
+use tokio_core::io::{copy, Io};
+use tokio_core::net::TcpListener;
+use futures::Future;
+use futures::stream::Stream;
+
 #[derive(Deserialize, Debug)]
 struct SnoopInputConfig {
     target: String,
@@ -14,6 +20,36 @@ struct SnoopOutput {
 
 impl Output for SnoopOutput {
     fn setup(&self, ctx: PluginContext) -> Result<Box<OutputInstance>> {
+        let ref mut core = ctx.core.try_borrow_mut()?;
+
+        let addr = "127.0.0.1:8080".parse::<SocketAddr>()
+            .map_err(|e| ErrorKind::Message(e.to_string()))?;
+        let handle = core.handle();
+
+        let socket = TcpListener::bind(&addr, &handle)?;
+
+        let done = socket.incoming()
+            .map_err(|_| ())
+            .for_each(move |(socket, addr)| {
+                let (reader, writer) = socket.split();
+                let amt = copy(reader, writer);
+
+                let msg = amt.then(move |result| {
+                    match result {
+                        Ok(amt) => println!("wrote {} bytes to {}", amt, addr),
+                        Err(e) => println!("error on {}: {}", addr, e),
+                    }
+
+                    Ok(())
+                });
+
+                handle.spawn(msg);
+
+                Ok(())
+            });
+
+        core.handle().spawn(done);
+
         Ok(Box::new(SnoopOutputInstance::new(ctx.id.clone())))
     }
 }
